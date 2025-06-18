75dd74e24bcfcbbf909ac41174551833d6bed35a
Marwan <marwanpsh@gmail.com>
2025-06-18 17:58:30 +0500

    api changed

--- client/src/api/CategoriesApi.js
+++ client/src/api/CategoriesApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/categories",
+  baseURL: "https://server-production-28d3.up.railway.app/api/categories",
 });
 
 export const getCategories = (id) => {
--- client/src/api/cart.js
+++ client/src/api/cart.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/cart",
+  baseURL: "https://server-production-28d3.up.railway.app/api/cart",
 });
 
 export const getCart = () => {
